23b4d0f2ce543aca478cb3cd00c14e2b2d5dc442
Muhammad Hammad <hammadirshad910@gmail.com>
2026-01-20 16:34:32 +0500

    created socketcontext.tsx and chat pages

--- context/SocketContext.tsx
+++ context/SocketContext.tsx
@@ -1,21 +1,41 @@
 'use client';
 
-import React, { useCallback } from 'react';
+import React, { useCallback, useContext, useEffect } from 'react';
+import { io } from 'socket.io-client';
 
 interface SocketContextProps {
     children?: React.ReactNode;
 }
 
-interface iSocketContext{
+interface iSocketContext {
     sendMessage: (msg: string) => any;
 }
 
+export const useSocket = () => {
+    const state = useContext(SocketContext);
+    if (!state) {
+        throw new Error("state is undefined...");
+    }
+    return state;
+}   
+
 const SocketContext = React.createContext<iSocketContext | null>(null);
 
-export const SocketProvider:React.FC<SocketContextProps> = ({ children }) => {
+export const SocketProvider: React.FC<SocketContextProps> = ({ children }) => {
 
-    const sendMessage:iSocketContext["sendMessage"] = useCallback((msg: string) => {
+    const sendMessage: iSocketContext["sendMessage"] = useCallback((msg: string) => {
         console.log('Sending message:', msg);
+    }, []);
+
+
+
+    useEffect(() => {
+        const _socket = io('http://localhost:3001');
+
+        return () => {
+            _socket.disconnect();
+        }
+
     }, []);
 
     return (
